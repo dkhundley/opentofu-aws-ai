c1bf946fd3fb45ebbd04c80d3aa3c8b73b047d19
dkhundley <david.k.hundley@gmail.com>
2024-02-02 20:01:14 -0600

    More changes

--- lambdas/lambda_ai/lambda_ai.py
+++ lambdas/lambda_ai/lambda_ai.py
@@ -1,15 +1,19 @@
 import os
 import json
 import boto3
+# from langchain_core.messages.human import HumanMessage
+# from langchain_community.chat_models import BedrockChat
 
 def lambda_handler(event, context):
 
-    # data = json.loads(event{"body"})
-
-    print('We got it.')
+    print(event['body'])
+    
+    my_dict = {
+        ''
+    }
     
     response = {
-        "body": json.dumps({"name": "David"}),
+        "body": json.dumps([{"generated_text": "David"}]),
         "isBase64Encoded": False,
         "statusCode": 200
     }
